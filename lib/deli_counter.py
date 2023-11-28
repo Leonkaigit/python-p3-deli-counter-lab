c2ec86a673574a91e8c014a10ab0675016c84614
LeonGitonga <leon.gitonga@student.moringaschool.com>
2023-11-28 07:47:23 +0300

    Lab done

---
+++ lib/deli_counter.py
@@ -0,0 +1,19 @@
+def line(deli_line):
+    if not deli_line:
+        print("The line is currently empty.")
+    else:
+        line_str = "The line is currently:"
+        for i, person in enumerate(deli_line, start=1):
+            line_str += f" {i}. {person}"
+        print(line_str)
+
+def take_a_number(deli_line, name):
+    deli_line.append(name)
+    print(f"Welcome, {name}. You are number {len(deli_line)} in line.")
+
+def now_serving(deli_line):
+    if not deli_line:
+        print("There is nobody waiting to be served!")
+    else:
+        serving = deli_line.pop(0)
+        print(f"Currently serving {serving}.")
